95d1ab6c17562f2c7c13432f3f9e3fcff4631b07
linhhn13 <linhhn13@fe.edu.vn>
2024-05-14 19:52:02 +0700

    change pass

--- Buoi1/Homeworks/hash_game.py
+++ Buoi1/Homeworks/hash_game.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 # Mảng chứa tất cả các chữ cái không viết hoa
 lowercase_letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
@@ -9,7 +10,7 @@ uppercase_letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
 # Hàm check_password(input_password: str) -> bool nhận vào một chuỗi input_password và trả về True nếu chuỗi đúng mật khẩu
 def check_password(input_password: str) -> bool:
     hashed_password = simple_hash_function(input_password)
-    if hashed_password == "1075eeff":
+    if hashed_password == "32aff7":
         return True
     return False
 
@@ -23,8 +24,25 @@ def simple_hash_function(input_string: str) -> str:
     return hashed_string
 
 def find_password():
-    password = "abcdef"
-    # TODO: Viết hàm tìm ra mật khẩu
+    characters = lowercase_letters
+    n = 0
+    for a1 in characters:
+        for a2 in characters:
+            for a3 in characters:
+                for a4 in characters:
+                    n = n + 1
+                    password = a1 + a2 + a3 + a4
+                    print(f"Lần thử thứ {n} {password}")
+                    os.system("clear")
+                    check_password(password)
+                    if check_password(password):
+                        print("Mật khẩu đúng")
+                        return password
+    print("Sai mật khẩu")
+    return ""
+            
+        
+    
     check_password(password)
     if check_password(password):
         print("Mật khẩu đúng")
